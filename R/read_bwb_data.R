778cda2f9d8b774a8dcb30d4537d364e35c852e7
mrustl <michael.rustler@kompetenz-wasser.de>
2019-02-13 15:02:20 +0100

    Fix CI (due to non-ASCII characters)

--- R/read_bwb_data.R
+++ R/read_bwb_data.R
@@ -12,8 +12,8 @@ get_site_id <- function(string, pattern = "^[0-9]{1,4}") {
 #' Helper function: gather_ignore
 #' 
 #' @param fields column names to be ignored for gathering (default: c(
-#'"Datum", "KN", "[iI]nterne Nr.", "Name der", "Ort", "Probe", "Prüf",
-#'"Untersuchung", "Labor", "Jahr", "Galer", "Detail", "Meß", "Zeit",
+#'"Datum", "KN", "[iI]nterne Nr.", "Name der", "Ort", "Probe", "Pr\u00F6f",
+#'"Untersuchung", "Labor", "Jahr", "Galer", "Detail", "Me\u00DF", "Zeit",
 #'"Bezei", "Monat")
 #' @return vector with ignored columns for gathering 
 #' @importFrom kwb.utils collapsed
@@ -21,8 +21,8 @@ get_site_id <- function(string, pattern = "^[0-9]{1,4}") {
 #' 
 gather_ignore <- function(
   fields = c("Datum", "KN", "[iI]nterne Nr.", "Name der", "Ort", "Probe", 
-"Prüf","Untersuchung", "Labor", "Jahr", "Galer", "Detail", 
-"Meß", "Zeit", "Bezei", "Monat") ) {
+"Pr\u00F6f","Untersuchung", "Labor", "Jahr", "Galer", "Detail", 
+"Me\u00DF", "Zeit", "Bezei", "Monat") ) {
   
 
   kwb.utils::collapsed(fields, "|")
