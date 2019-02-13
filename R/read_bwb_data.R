0e4f301387cd6d4d535a763b295f6ef230413c10
mrustl <michael.rustler@kompetenz-wasser.de>
2019-02-13 15:12:47 +0100

    Fix typo :bug:

--- R/read_bwb_data.R
+++ R/read_bwb_data.R
@@ -12,8 +12,8 @@ get_site_id <- function(string, pattern = "^[0-9]{1,4}") {
 #' Helper function: gather_ignore
 #' 
 #' @param fields column names to be ignored for gathering (default: c(
-#'"Datum", "KN", "[iI]nterne Nr.", "Name der", "Ort", "Probe", "Pr\u00F6f",
-#'"Untersuchung", "Labor", "Jahr", "Galer", "Detail", "Me\u00DF", "Zeit",
+#'"Datum", "KN", "[iI]nterne Nr.", "Name der", "Ort", "Probe", "Pr\\u00FC",
+#'"Untersuchung", "Labor", "Jahr", "Galer", "Detail", "Me\\u00DF", "Zeit",
 #'"Bezei", "Monat")
 #' @return vector with ignored columns for gathering 
 #' @importFrom kwb.utils collapsed
@@ -21,7 +21,7 @@ get_site_id <- function(string, pattern = "^[0-9]{1,4}") {
 #' 
 gather_ignore <- function(
   fields = c("Datum", "KN", "[iI]nterne Nr.", "Name der", "Ort", "Probe", 
-"Pr\u00F6f","Untersuchung", "Labor", "Jahr", "Galer", "Detail", 
+"Pr\u00FC","Untersuchung", "Labor", "Jahr", "Galer", "Detail", 
 "Me\u00DF", "Zeit", "Bezei", "Monat") ) {
   
 
