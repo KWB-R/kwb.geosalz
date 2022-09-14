0757485064571ca1292490c236bf33fb11ccb571
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-14 12:41:17 +0200

    Clean read_lab_bwb.R
    - reindent
    - use kwb.utils functions
      - stopFormatted()
      - stringList
    - use "not_available" instead of "!is_available"
    - extract stop_on_missing_sheets()

--- R/read_lab_bwb.R
+++ R/read_lab_bwb.R
@@ -10,13 +10,13 @@
 #' replace_nanb_with_na(string)
 #' @importFrom stringr str_replace
 
-replace_nanb_with_na <- function(string) {
-
-stringr::str_replace(string, 
-                     pattern = "n\\.\\s{0,10}(a|b)\\.", 
-                     replacement = NA_character_) 
-
-
+replace_nanb_with_na <- function(string)
+{
+  stringr::str_replace(
+    string, 
+    pattern = "n\\.\\s{0,10}(a|b)\\.", 
+    replacement = NA_character_
+  ) 
 }
 
 #' Read Master Data
@@ -25,31 +25,32 @@ stringr::str_replace(string,
 #' @return imported master data contained in sheet "Stammdaten"
 #' @export
 #'
+#' @importFrom kwb.utils stopFormatted stringList
 #' @importFrom readxl excel_sheets read_xlsx
 #' @importFrom janitor clean_names
-
-
-read_master_data <- function(path) {
-  
+read_master_data <- function(path)
+{
   sheets_needed <- "Stammdaten"
   
-  sheets_available <- readxl::excel_sheets(path)
+  stop_on_missing_sheets(path, sheets_needed)
   
-  is_available <- sheets_needed %in% sheets_available
-  if(!all(is_available)) {
-    msg <- sprintf("The following sheets are not available the Excel file %s:\n%s",
-                   path,
-                   paste0("'", sheets_needed[!is_available], "'", collapse = ", "))
-    stop(msg)
-  }
-  
-  
- readxl::read_xlsx(path,
-                   sheet =  sheets_needed) %>%
-   janitor::clean_names() %>% 
-   dplyr::rename(messstelle = .data$name)
-
+  readxl::read_xlsx(path, sheet = sheets_needed) %>%
+    janitor::clean_names() %>% 
+    dplyr::rename(messstelle = .data$name)
 }  
+
+stop_on_missing_sheets <- function(path, sheets_needed)
+{
+  not_available <- ! sheets_needed %in% readxl::excel_sheets(path)
+  
+  if (any(not_available)) {
+    kwb.utils::stopFormatted(
+      "The following sheets are not available the Excel file %s:\n%s",
+      path,
+      kwb.utils::stringList(sheets_needed[not_available])
+    )
+  }
+}
 
 #' Read Lab BWB
 #'
@@ -69,105 +70,116 @@ read_master_data <- function(path) {
 #' @importFrom dplyr filter if_else mutate relocate
 #' @importFrom tibble as_tibble
 
-read_lab_bwb <- function(path) {
-
-sheets_needed <- c("Stammdaten", "Analysen")
-
-sheets_available <- readxl::excel_sheets(path)
-
-is_available <- sheets_needed %in% sheets_available
-if(!all(is_available)) {
-  msg <- sprintf("The following sheets are not available the Excel file %s:\n%s",
-                 path,
-                 paste0("'", sheets_needed[!is_available], "'", collapse = ", "))
-  stop(msg)
+read_lab_bwb <- function(path)
+{
+  sheets_needed <- c("Stammdaten", "Analysen")
+  
+  stop_on_missing_sheets(path, sheets_needed)
+  
+  rows_to_skip <- 5L
+  
+  lab_bwb_01 <- readxl::read_xlsx(
+    path = path,
+    sheet = "Analysen",
+    range = cellranger::cell_limits(ul = c(rows_to_skip,1L), lr = c(NA, 53L))
+  )
+  
+  lab_bwb_02 <- readxl::read_xlsx(
+    path = path,
+    sheet = "Analysen",
+    range = cellranger::cell_limits(ul = c(rows_to_skip, 210L), lr = c(NA, 211L))
+  )
+  
+  lab_bwb <- dplyr::bind_cols(lab_bwb_01, lab_bwb_02) 
+  
+  # lab_bwb <- readxl::read_xlsx(
+  #   path = path,
+  #   sheet = "Analysen",
+  #   range = cellranger::cell_limits(ul = c(5L, 1L), lr = c(NA, 211L))
+  # )
+  
+  keyFields <- grep("@", names(lab_bwb), invert = TRUE, value = TRUE)
+  
+  result <- kwb.utils::hsMatrixToListForm(
+    as.data.frame(lab_bwb), keyFields,
+    colNamePar = "key", 
+    colNameVal = "par_val_org"
+  ) %>%  
+    tidyr::separate(
+      col = "key", 
+      into = c("par_name", "method", "par_name_phreeqc", "unit_org"),
+      sep = "@"
+    ) %>% 
+    janitor::clean_names() %>% 
+    dplyr::mutate(probenahme_datum = as.Date(.data$probenahme_datum))
+  
+  result$par_val <- replace_nanb_with_na(result$par_val_org) 
+  
+  idx_below_or_above_detection_limit <- which(
+    stringr::str_detect(result$par_val, pattern = "<|>")
+  )
+  
+  idx_style_german <- idx_below_or_above_detection_limit[
+    which(stringr::str_detect(
+      result$par_val[idx_below_or_above_detection_limit], 
+      pattern = ","
+    ))
+  ]
+  
+  if (length(idx_style_german) > 0L) {
+    result$par_val[idx_style_german] <- stringr::str_replace(
+      result$par_val[idx_style_german], ",", "."
+    )
+  }
+  
+  idx_style_english <- idx_below_or_above_detection_limit[
+    which(stringr::str_detect(
+      result$par_val[idx_below_or_above_detection_limit], 
+      pattern = "."
+    ))
+  ]
+  
+  stopifnot(
+    length(idx_style_english) == length(idx_below_or_above_detection_limit)
+  )
+  
+  result <- result %>%  
+    dplyr::bind_cols(kwb.base::hsLabValToVal(
+      result$par_val,
+      country = "en",
+      detLimFactorBelow = 0.5,
+      stopOnError = FALSE
+    )) %>%
+    janitor::clean_names() %>% 
+    dplyr::mutate(
+      unit = dplyr::if_else(
+        condition = .data$unit_org == "\u00B5g/l",
+        true = "mg/l",
+        false = .data$unit_org
+      ),
+      numeric_value = dplyr::if_else(
+        condition = .data$unit_org == "\u00B5g/l",
+        true = .data$numeric_value * 0.001,
+        false = .data$numeric_value
+      )
+    ) 
+  
+  character_paras <- c("Farbe", "Tr\u00FCbung", "Geruch", "Bodensatz")
+  
+  result %>% 
+    dplyr::filter(
+      .data$par_name %in% character_paras & !is.na(.data$par_val_org) |
+        !.data$par_name %in% character_paras & !is.na(.data$numeric_value)
+    ) %>% 
+    dplyr::mutate(
+      character_value = dplyr::if_else(
+        .data$par_name %in% character_paras,
+        true = .data$par_val_org, 
+        false = NA_character_
+      )
+    ) %>% 
+    dplyr::relocate(.data$character_value, .before = .data$numeric_value) %>% 
+    tibble::as_tibble() %>% 
+    dplyr::mutate(labor = "BWB") %>% 
+    dplyr::select(- .data$par_val)
 }
-
-
-
-rows_to_skip <- 5
-
-lab_bwb_01 <- readxl::read_xlsx(path = path,
-                             sheet = "Analysen",
-                             range = cellranger::cell_limits(ul = c(rows_to_skip,1), 
-                                                             lr = c(NA, 53)))
-
-lab_bwb_02 <- readxl::read_xlsx(path = path,
-                                sheet = "Analysen",
-                                range = cellranger::cell_limits(ul = c(rows_to_skip,210), 
-                                                                lr = c(NA, 211)))
-
-lab_bwb <- dplyr::bind_cols(lab_bwb_01, lab_bwb_02) 
-
-
-# lab_bwb <- readxl::read_xlsx(path = path,
-#                              sheet = "Analysen",
-#                              range = cellranger::cell_limits(ul = c(5,1), 
-#                                                              lr = c(NA, 211)))
-
-keyFields <- grep("@", names(lab_bwb), invert = TRUE, value = TRUE)
-result <- kwb.utils::hsMatrixToListForm(as.data.frame(lab_bwb), keyFields,
-                                        colNamePar = "key", 
-                                        colNameVal = "par_val_org") %>%  
-  tidyr::separate(col = "key", 
-                  into = c("par_name", "method", "par_name_phreeqc", "unit_org"),
-                  sep = "@") %>% 
-  janitor::clean_names() %>% 
-  dplyr::mutate(probenahme_datum = as.Date(.data$probenahme_datum))
-
-result$par_val <- replace_nanb_with_na(result$par_val_org) 
-
-
-idx_below_or_above_detection_limit <- which(stringr::str_detect(result$par_val,
-                                                                pattern = "<|>"))  
-
-
-idx_style_german <- idx_below_or_above_detection_limit[which(stringr::str_detect(result$par_val[idx_below_or_above_detection_limit], 
-                                        pattern = ","))]
-
-if (length(idx_style_german) > 0) {
-  result$par_val[idx_style_german] <- stringr::str_replace(result$par_val[idx_style_german], ",", ".")
-}
-
-
-idx_style_english <- idx_below_or_above_detection_limit[which(stringr::str_detect(result$par_val[idx_below_or_above_detection_limit], 
-                                                                                 pattern = "."))]
-
-
-stopifnot(length(idx_style_english) == length(idx_below_or_above_detection_limit))
-
-result <- result %>%  
-  dplyr::bind_cols(kwb.base::hsLabValToVal(result$par_val,
-                                           country = "en",
-                                           detLimFactorBelow = 0.5,
-                                           stopOnError = FALSE)
-                   ) %>%
-  janitor::clean_names() %>% 
-  dplyr::mutate(unit = dplyr::if_else(condition = .data$unit_org == "\u00B5g/l",
-                                      true = "mg/l",
-                                      false = .data$unit_org),
-                numeric_value = dplyr::if_else(condition = .data$unit_org == "\u00B5g/l",
-                                               true = .data$numeric_value * 0.001,
-                                               false = .data$numeric_value)
-                ) 
-
-character_paras <- c("Farbe", "Tr\u00FCbung", "Geruch", "Bodensatz")
-
-result <- result %>% 
-  dplyr::filter(.data$par_name %in% character_paras & !is.na(.data$par_val_org) |
-                !.data$par_name %in% character_paras & !is.na(.data$numeric_value)) %>% 
-  dplyr::mutate(character_value = dplyr::if_else(.data$par_name %in% character_paras,
-                                                 true = .data$par_val_org, 
-                                                 false = NA_character_)
-                ) %>% 
-  dplyr::relocate(.data$character_value, .before = .data$numeric_value) %>% 
-  tibble::as_tibble() %>% 
-  dplyr::mutate(labor = "BWB") %>% 
-  dplyr::select(- .data$par_val)
-
-
-return(result)
-}
-
-
-
