f56d4af659c08d6410419377805574c7e0051cc7
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-16 17:50:15 +0200

    Reindent, use kwb.utils::replaceFileExtension()

--- R/write_measurementchains_data.R
+++ R/write_measurementchains_data.R
@@ -12,77 +12,83 @@
 #'
 #' @importFrom archive archive_write_files
 #' @importFrom dplyr n summarise 
-#' @importFrom kwb.utils catAndRun
+#' @importFrom kwb.utils catAndRun replaceFileExtension
 #' @importFrom fs file_delete path_abs
 #' @importFrom readr write_csv
-write_measurementchains_data <- function(mc_data,
-                                         target_directory, 
-                                         to_zip = FALSE,
-                                         debug = FALSE) {
-  
-  
-mc_data_stats <- mc_data %>%  
-  dplyr::summarise(datetime_min = min(.data$datum_uhrzeit), 
-                   datetime_max = max(.data$datum_uhrzeit),
-                   number_of_samples = dplyr::n()) 
+#' @importFrom stringr str_c str_remove_all str_replace
 
-
-datetime_to_character <- function(datetime) {
+write_measurementchains_data <- function(
+    mc_data,
+    target_directory, 
+    to_zip = FALSE,
+    debug = FALSE
+) 
+{
+  mc_data_stats <- mc_data %>%  
+    dplyr::summarise(
+      datetime_min = min(.data$datum_uhrzeit), 
+      datetime_max = max(.data$datum_uhrzeit),
+      number_of_samples = dplyr::n()
+    ) 
   
+  datetime_to_character <- function(datetime) {
+    paste0(
+      as.character(min(mc_data$datum_uhrzeit)) %>%  
+        stringr::str_remove_all("-|:") %>% 
+        stringr::str_replace(" ", "-") %>% 
+        stringr::str_c("TZ"),
+      ifelse(attr(datetime, "tzone") == "Etc/GMT-1", "+01", "not-implemented")
+    )
+  }
   
-  paste0(
-  as.character(min(mc_data$datum_uhrzeit)) %>%  
-    stringr::str_remove_all("-|:") %>% 
-    stringr::str_replace(" ", "-") %>% 
-    stringr::str_c("TZ"),
-    ifelse(attr(datetime, "tzone") == "Etc/GMT-1", "+01", "not-implemented")
+  dataset_name <- deparse(substitute(expr = mc_data))
+  
+  csv_path <- sprintf(
+    "%s/mc-data_%s_%s.csv",
+    fs::path_abs(target_directory),
+    datetime_to_character(mc_data_stats$datetime_min),
+    datetime_to_character(mc_data_stats$datetime_max)
   )
-}
-
-dataset_name <- deparse(substitute(expr = mc_data))
-
-csv_path <- sprintf("%s/mc-data_%s_%s.csv",
-                    fs::path_abs(target_directory),
-                    datetime_to_character(mc_data_stats$datetime_min),
-                    datetime_to_character(mc_data_stats$datetime_max))
-
-
-msg <- sprintf("Exporting provided dataset '%s' to '%s'",
-               dataset_name, 
-               csv_path)
-
-kwb.utils::catAndRun(messageText = msg, 
-                     expr =  {
-                       readr::write_csv(mc_data, file = csv_path)
-                     },
-                     dbg = debug
-)
-
-path <- csv_path
-
-if(to_zip) {
-  zip_path <- stringr::str_replace(csv_path, 
-                                  "\\.csv", 
-                                  ".zip")
   
-  msg <- sprintf("Exporting provided dataset '%s' to '%s' and delete intermediate '%s'",
-                 dataset_name, 
-                 zip_path, 
-                 csv_path)
-  
-  kwb.utils::catAndRun(messageText = msg, 
-                       expr =  {
-                         archive::archive_write_files( 
-                           archive = zip_path,
-                           files = csv_path)
-                         fs::file_delete(path = csv_path)
-                         path <- zip_path
-                       },
-                       dbg = debug
+  msg <- sprintf(
+    "Exporting provided dataset '%s' to '%s'",
+    dataset_name, 
+    csv_path
   )
-
-}
-
-
-path
+  
+  kwb.utils::catAndRun(
+    messageText = msg, 
+    expr =  {
+      readr::write_csv(mc_data, file = csv_path)
+    },
+    dbg = debug
+  )
+  
+  path <- csv_path
+  
+  if (to_zip) {
+    
+    zip_path <- kwb.utils::replaceFileExtension(csv_path, ".zip")
+    
+    msg <- sprintf(
+      "Exporting provided dataset '%s' to '%s' and delete intermediate '%s'",
+      dataset_name, 
+      zip_path, 
+      csv_path
+    )
+    
+    kwb.utils::catAndRun(
+      messageText = msg, 
+      expr =  {
+        archive::archive_write_files( 
+          archive = zip_path,
+          files = csv_path)
+        fs::file_delete(path = csv_path)
+        path <- zip_path
+      },
+      dbg = debug
+    )
+  }
+  
+  path
 }
